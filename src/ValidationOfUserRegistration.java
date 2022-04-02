795d10aeb898c9c7f3720fd17ca55495fccf17b0
shaikhsirin <shaikhsirin8080example.com>
2022-04-02 15:57:57 +0530

    Added:- Password validation with Rule Has to be speecial character

--- src/ValidationOfUserRegistration.java
+++ src/ValidationOfUserRegistration.java
@@ -68,4 +68,8 @@ public class ValidationOfUserRegistration {
         String regex = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9._@#$%&^*()+:?]{8,}$";
         return (password.matches(regex));
     }
+    public boolean validatePasswordRuleFour(String password) {
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[._@#$%&^*()+:?])[A-Za-z0-9._@#$%&^*()+:?]{8,}$";
+        return (password.matches(regex));
+    }
 }
