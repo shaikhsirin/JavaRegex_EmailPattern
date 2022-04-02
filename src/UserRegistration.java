1de2033efeee40d6232d9dc6c5eec05b30c70b16
shaikhsirin <shaikhsirin8080example.com>
2022-04-02 15:12:48 +0530

    Added:- Validation Of Last Name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -2,6 +2,7 @@ public class UserRegistration {
     public static void main(String[] args) {
         ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
         validationOf.validateFirstName();
+        validationOf.validateLastName();
     }
 
 }
