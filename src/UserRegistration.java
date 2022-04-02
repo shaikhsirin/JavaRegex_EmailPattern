4c50e444fcec228cd6b19ab16ffb492d2978cec8
shaikhsirin <shaikhsirin8080example.com>
2022-04-02 15:28:10 +0530

    Added:-Validation Of Mobile Number

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,9 +1,10 @@
 public class UserRegistration {
     public static void main(String[] args) {
         ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
-        validationOf.validateFirstName();
-        validationOf.validateLastName();
-        validationOf.validateEmailId();
+        validationOf.validateFirstName("Sirin");
+        validationOf.validateLastName("Shaikh");
+        validationOf.validateEmailId("sirinshaikh942@gmail.com");
+        validationOf.validateMobileNumber("91 96998080895");
     }
 
 }
