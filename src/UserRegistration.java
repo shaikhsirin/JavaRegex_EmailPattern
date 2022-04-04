c154789c40248ccd15c529a19fc05fedebf2c9b9
Sirin Shaikh <97180903+shaikhsirin@users.noreply.github.com>
2022-04-04 09:28:40 +0530

    Update UserRegistration.java

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -5,12 +5,12 @@ public class UserRegistration {
         ArrayList<String> email = new ArrayList<>();
         UserInputOfEmails emails = new UserInputOfEmails();
         ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
-//      validationOf.validateFirstName("Kiran");
-//      validationOf.validateLastName("Kumar");
-//      validationOf.validateEmailId("makamkiran95@gmail.com");
-//      validationOf.validateMobileNumber("91 9666069291");
-//      validationOf.validatePasswordRuleOne("kiran56@)+");
-//      System.out.println(validationOf.validatePasswordRuleFour("kirA@2nhhjj21hg"));
+//      validationOf.validateFirstName("Sirin");
+//      validationOf.validateLastName("Shaikh");
+//      validationOf.validateEmailId("sirinshaikh942@gmail.com");
+//      validationOf.validateMobileNumber("91 9699808095");
+//      validationOf.validatePasswordRuleOne("sirin56@)+");
+//      System.out.println(validationOf.validatePasswordRuleFour("sirA@2nhhjj21hg"));
         email = emails.userDataOfEmails();
         for(String e : email){
             validationOf.validateEmailId(e);
